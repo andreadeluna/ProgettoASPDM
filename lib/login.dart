949cd388f4841b6f9b462de8a0656c72bf565b4a
Andrea De Luna <andrea.deluna10@gmail.com>
2021-12-24 00:59:35 +0100

    Implementa la validazione sui dati inseriti nel form di login e di registrazione

--- lib/login.dart
+++ lib/login.dart
@@ -13,42 +13,46 @@ class Login extends StatelessWidget {
     final authService = Provider.of<Authentication>(context);
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: "Email",
+      body: Form(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                  ),
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: "Password",
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                  ),
+                ),
               ),
-            ),
+              ElevatedButton(
+                onPressed: (){
+                    authService.signInWithEmailAndPassword(
+                        emailController.text,
+                        passwordController.text);
+                },
+                child: const Text('Login'),
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.pushNamed(context, '/register');
+                },
+                child: Text('Register'),
+              )
+            ],
           ),
-          ElevatedButton(
-            onPressed: (){
-              authService.signInWithEmailAndPassword(
-                  emailController.text,
-                  passwordController.text);
-            },
-            child: const Text('Login'),
-          ),
-          ElevatedButton(
-            onPressed: (){
-              Navigator.pushNamed(context, '/register');
-            },
-            child: Text('Register'),
-          )
-        ],
+        ),
       ),
     );
   }
--- lib/register.dart
+++ lib/register.dart
@@ -1,50 +1,102 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:progettoaspdm/login.dart';
 import 'package:progettoaspdm/services/authentication.dart';
 import 'package:provider/provider.dart';
 
 class Register extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final TextEditingController emailController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
 
     final authService = Provider.of<Authentication>(context);
 
+    final GlobalKey<FormState> _key = GlobalKey<FormState>();
+
+    String errorMessage = '';
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: "Email",
+      body: Form(
+        key: _key,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: emailController,
+                  validator: validateEmail,
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                  ),
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: "Password",
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  validator: validatePassword,
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                  ),
+                ),
               ),
-            ),
+              // Center(
+              //   child: Text(errorMessage),
+              // ),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_key.currentState!.validate()) {
+                    // try{
+                      await authService.createUserWithEmailAndPassword(
+                          emailController.text, passwordController.text);
+                      Navigator.pop(context);
+                      // errorMessage = '';
+                    // } on FirebaseAuthException catch (error){
+                    //   errorMessage = error.message!;
+                    // }
+                  }
+                },
+                child: const Text('Register'),
+              ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () async {
-              await authService.createUserWithEmailAndPassword(
-                  emailController.text,
-                  passwordController.text);
-              Navigator.pop(context);
-            },
-            child: const Text('Register'),
-          ),
-        ],
+        ),
       ),
     );
   }
+}
+
+String? validateEmail(String? formEmail){
+  if(formEmail == null || formEmail.isEmpty){
+    return "L'indirizzo e-mail è richiesto";
+  }
+
+  String pattern = r'\w+@\w+\.\w+';
+  RegExp regex = RegExp(pattern);
+
+  if(!regex.hasMatch(formEmail)){
+    return "Formato indirizzo e-mail non valido";
+  }
+
+  return null;
+}
+
+
+String? validatePassword(String? formPassword) {
+  if (formPassword == null || formPassword.isEmpty) {
+    return "La password è richiesta";
+  }
+
+  String pattern =
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+  RegExp regex = RegExp(pattern);
+
+  if (!regex.hasMatch(formPassword)) {
+    return "La password deve essere di almeno 8 caratteri e deve contenere una lettera maiuscola, un numero e un simbolo";
+  }
+
+  return null;
 }
